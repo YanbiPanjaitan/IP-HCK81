96393c827b01b8cf3c5337030adb756024a60de3
YanbiPanjaitan <yanbi.2001@gmail.com>
2025-03-25 06:48:11 +0700

    feat: migrations and models

---
+++ servers/migrations/20250324234726-create-recommendation.js
@@ -0,0 +1,34 @@
+'use strict';
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable('Recommendations', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      FavoriteId: {
+        type: Sequelize.INTEGER
+      },
+      landmarkName: {
+        type: Sequelize.STRING
+      },
+      description: {
+        type: Sequelize.TEXT
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable('Recommendations');
+  }
+};
